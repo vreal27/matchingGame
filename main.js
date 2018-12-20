8d18a17202e17f3dd504ec8cb517f83b2a83f122
Eddie Villareal <eddie.villareal27@gmail.com>
2018-12-20 15:09:05 -0800

    displaying cards

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
+	var container = document.querySelector("#pics")
+	var cardTemplate = ""
+	var cardsArray = [
+	{pic:"https://image.flaticon.com/icons/svg/235/235405.svg"}, { pic: "https://image.flaticon.com/icons/svg/235/235349.svg"}, {pic:"https://image.flaticon.com/icons/svg/1303/1303576.svg"}, {pic:"https://image.flaticon.com/icons/svg/235/235349.svg"},{pic:"https://image.flaticon.com/icons/svg/235/235405.svg"},{pic:"https://image.flaticon.com/icons/svg/1303/1303576.svg"},{pic:"https://image.flaticon.com/icons/svg/578/578372.svg"},{pic:"https://image.flaticon.com/icons/svg/235/235361.svg"},{pic:"https://image.flaticon.com/icons/svg/578/578372.svg"},{pic:"https://image.flaticon.com/icons/svg/235/235361.svg"},{pic:"https://image.flaticon.com/icons/svg/1198/1198062.svg"},{pic:"https://image.flaticon.com/icons/svg/1363/1363376.svg"},{pic:"https://image.flaticon.com/icons/svg/1198/1198062.svg"},{pic:"https://image.flaticon.com/icons/svg/235/235396.svg"},{pic:"https://image.flaticon.com/icons/png/512/523/523445.png"},{pic:"https://image.flaticon.com/icons/svg/235/235396.svg"},{pic:"https://image.flaticon.com/icons/svg/1363/1363376.svg"},{pic:"https://image.flaticon.com/icons/png/512/523/523445.png"}]
+
+	cardsArray.forEach(function(card){
 	
+		cardTemplate += `
+		<div class="randomPic">
+			<p><img src="${card.pic}"/></p>
+		</div>
+
+		`	
+	})
+
+container.innerHTML = cardTemplate
 })
+
+
+
+
+// picturesTemplates 
+// 	${pictures.name}
